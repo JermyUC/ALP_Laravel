d6b25c0b2b863a9d0068b0bcf07d51837bbb7a31
MaverickEffenberg <bapak321bapak@gmail.com>
2025-11-10 21:29:15 +0700

    split layout into three
    split the layout.blade.php into three files: layout.blade.php, header.blade.php and footer.blade.php and include the header and footer into layout using @include

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+    <a class="navbar-brand brand" href="{{ url('/') }}">My Carnivlora</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="nav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('/store') }}">Store</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('/guide') }}">Guide</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About Us</a></li>
+        </ul>
+    </div>
+</nav>
